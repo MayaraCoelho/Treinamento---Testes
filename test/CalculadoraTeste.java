542286a668784d65bcd8debf4c5c70b3d7049034
uff <uff@sti01>
2016-04-15 16:16:54 -0300

    Aprendendo Testes

---
+++ test/CalculadoraTeste.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author uff
+ */
+public class CalculadoraTeste {
+    
+    public CalculadoraTeste() {
+    }
+    
+    @Test
+    public void deveriaSomarDoisValoresPassados() throws Exception 
+    {
+        int valorA = 1;
+	int valorB = 2;
+	int soma = 0;
+
+	assertEquals(3, soma);
+
+    }
+    
+//    @BeforeClass
+//    public static void setUpClass() {
+//    }
+//    
+//    @AfterClass
+//    public static void tearDownClass() {
+//    }
+//    
+//    @Before
+//    public void setUp() {
+//    }
+//    
+//    @After
+//    public void tearDown() {
+//    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
